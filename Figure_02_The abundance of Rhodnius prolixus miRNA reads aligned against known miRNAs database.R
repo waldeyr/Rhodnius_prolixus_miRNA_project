d0bb9a8fae7dba90d659f7eee0e96e0c840ab88a
waldeyr <waldeyr@gmail.com>
2023-02-06 09:50:43 -0300

    scripts and plots;

--- Figure_02_The abundance of Rhodnius prolixus miRNA reads aligned against known miRNAs database.R
+++ Figure_02_The abundance of Rhodnius prolixus miRNA reads aligned against known miRNAs database.R
@@ -30,19 +30,21 @@ plot <- Known_miRNA %>%
  filter(!(`predicted mature seq. in accordance with miRBase mature seq.` %in% "NA")) %>%
  ggplot() +
  aes(x = `total read count`, y = Species, fill = Tissue) +
- geom_boxplot(alpha=.65) +
- scale_fill_viridis_d(option = "viridis", 
- direction = 1) +
- scale_x_continuous(trans = "log10") +
+ geom_boxplot(alpha=.25) +
+ geom_dotplot(binaxis = "y", stackdir = "center", dotsize = .2, alpha=0.6)+
+ scale_fill_viridis_d(option = "viridis", direction = 1) +
+ scale_x_continuous(trans = "log10", breaks = c(100, 1000, 10000, 100000, 1000000)) +
   xlab(expression(paste("Abundance of ", italic("Rhodnius prolixus"), " miRNA reads")))+
  theme_light() +
  theme(
    legend.position = "none",
    text = element_text(size = 20),
-   axis.text.y = element_text(face = "italic")
+   axis.text.y = element_text(face = "italic"),
+   axis.text.x = element_text(size=10, angle=60, hjust=1)
    ) +
  facet_wrap(vars(Tissue))
 plot
-ggsave(file = file.path(result_basepath, "Plots", "Abundance of Rhodnius prolixus miRNA reads.pdf"), plot, width = 300, height = 360, units = "mm")
+ggsave(file = file.path(result_basepath, "Plots", "Figure_02_The abundance of Rhodnius prolixus miRNA reads aligned against known
+miRNAs database.pdf"), plot, width = 360, height = 360, units = "mm")
 
-
+save.image(file='Environment.RData')
